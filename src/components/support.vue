b0ba4034ecadbc092b3f9b6459be246099828a0e
pablofullana <pablofullana@gmail.com>
2017-06-08 12:15:18 -0300

    Add temp. donate button

--- src/components/support.vue
+++ src/components/support.vue
@@ -3,9 +3,10 @@
   <div id="heading" class="content">
     <div class="subtitle">Support Us</div>
     <hr />
-    <p class="is-small">
-      <a href="https://twitter.com/share" class="twitter-share-button" data-text="Pimp your mocoLUFA! via @yaeltex at" :data-url="host" data-hashtags="mocolufa" data-related="" data-dnt="true" data-show-count="false">Tweet</a>
-    </p>
+    <div class="block">
+      <a href="https://twitter.com/share" class="twitter-share-button" data-text="Pimp your mocoLUFA! via @yaeltex at" :data-url="host" data-hashtags="mocolufa" data-related="" data-dnt="true" data-size="large" data-show-count="false">Tweet</a>
+      <a class="button is-small is-warning"><strong>Donate</strong></a>
+    </div>
   </div>
 </template>
 
