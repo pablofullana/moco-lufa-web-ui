51c9afebb59b9f82ae5a9f65733edb0e900180d0
pablofullana <pablofullana@gmail.com>
2017-06-08 11:10:04 -0300

    Enable download

--- src/components/compiler.vue
+++ src/components/compiler.vue
@@ -6,7 +6,7 @@
     <form v-on:submit.prevent="compile">
       <div class="field is-horizontal">
         <div class="field-label is-small">
-          <label class="label">Device</label>
+          <label class="label">Device Name</label>
         </div>
         <div class="field-body">
           <div class="field">
@@ -47,18 +47,43 @@
       </div>
 
       <div class="field is-horizontal">
-        <div class="field-label">
+        <div class="field-label is-small">
           <!-- Left empty for spacing -->
         </div>
         <div class="field-body">
           <div class="field">
             <div class="control">
-              <input class="button is-primary is-small" type="submit" value="Compile"/>
+              <input class="button is-primary is-small" type="submit" value="Compile" :disabled="compilationProcess.status == 'inProgress'"/>
             </div>
           </div>
         </div>
       </div>
-      <p>{{ compilationResult }}</p>
+
+      <div class="field is-horizontal" v-if="compilationProcess.status === 'succeed'">
+        <div class="field-label is-small">
+          <label class="label">Summary</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <small>{{ compilationProcess.summary }}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal" v-if="compilationProcess.status === 'failed'">
+        <div class="field-label is-small">
+          <label class="label">Error</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control">
+              <small>{{ compilationProcess.summary }}</small>
+            </div>
+          </div>
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -70,11 +95,19 @@ export default {
   name: 'compiler',
   data: function () {
     return {
-      compilationResult: ''
+      compilationProcess: {
+        status: '',
+        hex: '',
+        summary: ''
+      }
     }
   },
   methods: {
     compile: function () {
+      var startedAtTime = new Date().getTime()
+      this.compilationProcess.status = 'inProgress'
+      this.compilationProcess.hex = ''
+      this.compilationProcess.summary = ''
       axios.post(process.env.FIRMWARES_API_ENDPOINT, {
         'firmware': {
           'name': this.name,
@@ -82,10 +115,35 @@ export default {
           'arduino_model': this.arduino_model
         }
       }).then((response) => {
-        this.compilationResult = response.data
+        var totalTime = new Date().getTime() - startedAtTime
+        var totalSeconds = (totalTime / 1000).toFixed(2)
+        this.compilationProcess.status = 'succeed'
+        this.compilationProcess.hex = response.data
+        this.compilationProcess.summary = String.concat('Compiled successfully in ', totalSeconds, ' seconds.')
+        this.download(this.name, this.compilationProcess.hex)
       }, (err) => {
-        console.log(err)
+        this.compilationProcess.status = 'failed'
+        this.compilationProcess.hex = ''
+        this.compilationProcess.summary = String.concat(err.response.status, ': ', err.response.statusText)
       })
+    },
+    download: function (name, fileContent) {
+      var filename = String.toLowerCase(name.replace(/[^\w]/gi, '_'))
+      var blob = new Blob([fileContent], { type: 'text/plain' })
+      var URL = window.URL || window.webkitURL
+      var downloadUrl = URL.createObjectURL(blob)
+      // use HTML5 a[download] attribute to specify filename
+      var a = document.createElement('a')
+      // safari doesn't support this yet
+      if (typeof a.download === 'undefined') {
+        window.location = downloadUrl
+      } else {
+        a.href = downloadUrl
+        a.download = String.concat(filename, '.hex')
+        document.body.appendChild(a)
+        a.click()
+      }
+      setTimeout(function () { URL.revokeObjectURL(downloadUrl) }, 100) // cleanup
     }
   }
 }
