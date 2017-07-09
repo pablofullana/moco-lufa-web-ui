076c115aeb17ef61eb982fd9fbf15f1a49c68f26
pablofullana <pablofullana@gmail.com>
2017-07-09 15:12:29 -0300

    Update support buttons - Use custom Twitter link - Use PayPal.me instead of PayPal

--- src/components/support.vue
+++ src/components/support.vue
@@ -3,21 +3,22 @@
   <div id="heading" class="content">
     <div class="subtitle">Support Us</div>
     <hr />
-    <div class="block">
-      <div class="columns is-vcentered">
-        <div class="column">
-          <a href="https://twitter.com/share" class="twitter-share-button" data-text="Pimp your mocoLUFA! via @yaeltex at" :data-url="host" data-hashtags="mocolufa" data-related="" data-dnt="true" data-size="large" data-show-count="false">Tweet</a>
-        </div>
-        <div class="column">
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="MKY8SNBJ6RSHC">
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-          </form>
-        </div>
-      </div>
-    </div>
+      <a class="button support is-info is-small" data-social-network="Twitter" data-social-action="tweet" target="_blank" :href="'https://twitter.com/intent/tweet?text=Pimp your mocoLUFA! via @yaeltex at: ' + host">
+        <span class="is-vcentered">
+          <span class="icon is-small">
+            <i class="fa fa-twitter"></i>
+          </span>
+          <span>Tweet</span>
+        </span>
+      </a>
+      <a class="button support is-warning is-small" href="https://www.paypal.me/pablofullana" target="_blank">
+        <span class="is-vcentered">
+          <span class="icon is-small">
+            <i class="fa fa-heart-o"></i>
+          </span>
+          <span>Donate</span>
+        </span>
+      </a>
   </div>
 </template>
 
