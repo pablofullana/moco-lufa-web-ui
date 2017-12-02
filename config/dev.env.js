95bc1450809bbf3840e6db4563f38a5d459684b4
pablofullana <pablofullana@gmail.com>
2017-12-02 17:37:45 -0300

    Update README file

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,5 +6,6 @@ module.exports = merge(prodEnv, {
 
   SERVER_SETUP_API_ENDPOINT:  '"http://192.168.0.5:3000/api/v1/server_setup"',
   FIRMWARES_API_ENDPOINT:     '"http://192.168.0.5:3000/api/v1/firmwares"',
-  STATS_API_ENDPOINT:         '"http://192.168.0.5:3000/api/v1/stats"'
+  STATS_API_ENDPOINT:         '"http://192.168.0.5:3000/api/v1/stats"',
+  BTC_WALLET_ADDRESS:         '"0123456789012345678901234567890123"'
 })
